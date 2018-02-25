e96dc61a30640aca88236db0d1d385ba6113deef
Evan Candler <emcandler21@gmail.com>
2018-02-25 20:05:54 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,10 +1,10 @@
 # code here!
 class School
   attr_accessor :school_name, :roster
-  
+
   def initialize(school_name)
     @school_name = school_name
-    roster = {}
+    @roster = {}
   end
 
   def roster
